430efe43419f349382aab94b0e776afd060fd9d1
Andrei Pustovoit <andreipustovoit6@gmail.com>
2025-05-15 20:29:58 +0300

    add components header

---
+++ Front-end/src/app/Header/language-btn/language-btn.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-language-btn',
+  imports: [],
+  templateUrl: './language-btn.component.html',
+  styleUrl: './language-btn.component.css'
+})
+export class LanguageBtnComponent {
+
+}
---
+++ Front-end/src/app/Header/theme-btn/theme-btn.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-theme-btn',
+  imports: [],
+  templateUrl: './theme-btn.component.html',
+  styleUrl: './theme-btn.component.css'
+})
+export class ThemeBtnComponent {
+
+}
