34aedbfe9d1b56ae4e519327939b6c38b5ee2a30
Andrei Pustovoit <andreipustovoit6@gmail.com>
2025-05-15 22:48:34 +0300

    add all components in header-bar and add cssvariable

--- Front-end/src/app/Header/header-bar/header-bar.component.ts
+++ Front-end/src/app/Header/header-bar/header-bar.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { LogoComponent } from '../logo/logo.component';
+import { SearchComponent } from '../search/search.component';
+import { ThemeBtnComponent } from '../theme-btn/theme-btn.component';
+import { LanguageBtnComponent } from '../language-btn/language-btn.component';
+import { SinginAdminComponent } from '../singin-admin/singin-admin.component';
 
 @Component({
   selector: 'app-header-bar',
-  imports: [],
+  imports: [LogoComponent, SearchComponent, ThemeBtnComponent, LanguageBtnComponent, SinginAdminComponent],
   templateUrl: './header-bar.component.html',
   styleUrl: './header-bar.component.css'
 })
