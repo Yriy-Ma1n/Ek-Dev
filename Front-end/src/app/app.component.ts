e11dc2f23423c22908350eb2c8f974a4dff787e5
Ma1n <kskiller2020@gmail.com>
2025-05-16 20:34:18 +0300

    create News component

--- Front-end/src/app/app.component.ts
+++ Front-end/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderBarComponent } from './Header/header-bar/header-bar.component';
@@ -11,22 +11,13 @@ import { HeaderBarComponent } from './Header/header-bar/header-bar.component';
 })
 export class AppComponent {
   http = inject(HttpClient)
-  limit:number = 5
-  connected(){
-      this.http.get(`http://localhost:5000/products`).subscribe({
-        next:(res)=>console.log(res),
-        error:(err)=>console.log(err)
-      })
+
+  connectToReview(){
+    this.http.get(`http://localhost:5500/review`).subscribe(data=>{
+      console.log(data)
+    })
   }
-  popularProduct(){
-     this.http.get(`http://localhost:5000/PopularModel?limit=${this.limit}`).subscribe({
-        next:(res)=>console.log(res),
-        error:(err)=>console.log(err)
-      })
-  }
-  takeCount(event:Event){
-   const element= event.target as HTMLInputElement
-   this.limit = Number(element.value) || 5
-  }
+ 
+ 
  
 }
