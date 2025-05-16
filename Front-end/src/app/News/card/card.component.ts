94d6aec41dd39f66605cd93ae6b06a0ac9cba7dd
Ma1n <kskiller2020@gmail.com>
2025-05-16 21:03:03 +0300

    add card from backend

--- Front-end/src/app/News/card/card.component.ts
+++ Front-end/src/app/News/card/card.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
 export class CardComponent {
-
+    router = inject(HttpClient)
+    tovar:{_id:string, img:string, name:string, description:string}[] = []
+    ngOnInit(){
+      this.router.get<{_id: string, img: string, name: string, description: string}[]>('http://localhost:5500/review').subscribe(tovar=>{
+        this.tovar = tovar
+      })
+    }
 }
