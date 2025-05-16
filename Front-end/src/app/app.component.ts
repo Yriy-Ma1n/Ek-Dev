b713a26c7fc9ee39db9adb7af901a880097a2dc9
Andrei Pustovoit <andreipustovoit6@gmail.com>
2025-05-16 23:30:29 +0300

    add logo in component

--- Front-end/src/app/app.component.ts
+++ Front-end/src/app/app.component.ts
@@ -10,23 +10,5 @@ import { HeaderBarComponent } from './Header/header-bar/header-bar.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  http = inject(HttpClient)
-  limit:number = 5
-  connected(){
-      this.http.get(`http://localhost:5000/products`).subscribe({
-        next:(res)=>console.log(res),
-        error:(err)=>console.log(err)
-      })
-  }
-  popularProduct(){
-     this.http.get(`http://localhost:5000/PopularModel?limit=${this.limit}`).subscribe({
-        next:(res)=>console.log(res),
-        error:(err)=>console.log(err)
-      })
-  }
-  takeCount(event:Event){
-   const element= event.target as HTMLInputElement
-   this.limit = Number(element.value) || 5
-  }
- 
+  
 }
