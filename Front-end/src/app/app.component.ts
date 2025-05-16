6aad28ccf26b7caf18376ae5d35312e2b9f73cbc
Ma1n <kskiller2020@gmail.com>
2025-05-16 13:00:08 +0300

    add backend

--- Front-end/src/app/app.component.ts
+++ Front-end/src/app/app.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HttpClientModule],
+  standalone:true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Front-end';
+  http = inject(HttpClient)
+  limit:number = 5
+  connected(){
+      this.http.get(`http://localhost:5000/products`).subscribe({
+        next:(res)=>console.log(res),
+        error:(err)=>console.log(err)
+      })
+  }
+  popularProduct(){
+     this.http.get(`http://localhost:5000/PopularModel?limit=${this.limit}`).subscribe({
+        next:(res)=>console.log(res),
+        error:(err)=>console.log(err)
+      })
+  }
+  takeCount(event:Event){
+   const element= event.target as HTMLInputElement
+   this.limit = Number(element.value) || 5
+  }
+ 
 }
