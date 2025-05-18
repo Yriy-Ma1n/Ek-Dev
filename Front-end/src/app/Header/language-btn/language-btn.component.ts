5bbc3e819c3a1f134de5597fe539728e32fb204c
Andrei Pustovoit <andreipustovoit6@gmail.com>
2025-05-18 16:11:27 +0300

    add button language on page and animation arrow

--- Front-end/src/app/Header/language-btn/language-btn.component.ts
+++ Front-end/src/app/Header/language-btn/language-btn.component.ts
@@ -1,11 +1,21 @@
+import { NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-language-btn',
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './language-btn.component.html',
   styleUrl: './language-btn.component.css'
 })
 export class LanguageBtnComponent {
-
+  transformArrow = true;
+  transformValue = "";
+ onClick(){
+  if(this.transformArrow){
+    this.transformValue = "rotate(180deg)";
+  }else{
+    this.transformValue = "rotate(0deg)";
+  }
+   return this.transformArrow = !this.transformArrow;
+ }
 }
