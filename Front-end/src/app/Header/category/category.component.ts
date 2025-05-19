55270d728cafcde8d1cb0149ab06df12ae8b6d53
Ma1n <kskiller2020@gmail.com>
2025-05-19 15:50:47 +0300

    add icon for Gadgets

--- Front-end/src/app/Header/category/category.component.ts
+++ Front-end/src/app/Header/category/category.component.ts
@@ -1,15 +1,25 @@
+import { NgClass} from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-category',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './category.component.html',
   styleUrl: './category.component.css'
 })
 export class CategoryComponent {
+  hovered:boolean = true
+
+  listCategoryItem:{img:string, name:string}[] = []
+
   hoverToElemet(event:Event){
     const element = event.target as HTMLElement
     if(element.classList.contains("list-category-container")) return
-    console.log(element)
+    this.hovered = false
+    element.classList.contains("Gadgets")
+  }
+  leaveOfElement(){
+    this.hovered = true
+    
   }
 }
