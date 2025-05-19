aefb33ebf2761c998d4c7dacaa3d4f88598fc7f1
Ma1n <kskiller2020@gmail.com>
2025-05-19 15:10:18 +0300

    add check for event

--- Front-end/src/app/Header/category/category.component.ts
+++ Front-end/src/app/Header/category/category.component.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './category.component.css'
 })
 export class CategoryComponent {
-
+  hoverToElemet(event:Event){
+    const element = event.target as HTMLElement
+    if(element.classList.contains("list-category-container")) return
+    console.log(element)
+  }
 }
--- Front-end/src/app/Header/header-bar/header-bar.component.ts
+++ Front-end/src/app/Header/header-bar/header-bar.component.ts
@@ -5,10 +5,11 @@ import { ThemeBtnComponent } from '../theme-btn/theme-btn.component';
 import { LanguageBtnComponent } from '../language-btn/language-btn.component';
 import { SinginAdminComponent } from '../singin-admin/singin-admin.component';
 import { CategoryListComponent } from '../category-list/category-list.component';
+import { CategoryComponent } from '../category/category.component';
 
 @Component({
   selector: 'app-header-bar',
-  imports: [LogoComponent, SearchComponent, ThemeBtnComponent, LanguageBtnComponent, SinginAdminComponent, CategoryListComponent],
+  imports: [LogoComponent, SearchComponent, ThemeBtnComponent, LanguageBtnComponent, SinginAdminComponent, CategoryListComponent, CategoryComponent],
   templateUrl: './header-bar.component.html',
   styleUrl: './header-bar.component.css'
 })
