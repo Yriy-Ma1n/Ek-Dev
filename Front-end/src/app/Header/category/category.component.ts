8de61f759bd1459abd2392d85f4ee98363bd663e
Ma1n <kskiller2020@gmail.com>
2025-05-20 14:01:12 +0300

    add TV

--- Front-end/src/app/Header/category/category.component.ts
+++ Front-end/src/app/Header/category/category.component.ts
@@ -19,27 +19,10 @@ export class CategoryComponent {
 
     if (element.classList.contains("Gadgets")) {
       this.listCategoryItem = []
-      this.listCategoryItem.push({ img: './header-list/Gadgets/iphone-svg.svg', name: "Смартфони" })
-      this.listCategoryItem.push({ img: './header-list/Gadgets/iphone-box.svg', name: "Чохли" })
-      this.listCategoryItem.push({ img: './header-list/Gadgets/head-set.svg', name: "Навушники" })
-      this.listCategoryItem.push({ img: './header-list/Gadgets/smart-watch.svg', name: "Смартгодинники і браслети" })
-      this.listCategoryItem.push({ img: './header-list/Gadgets/power-bank.svg', name: "Повербанки" })
-      this.listCategoryItem.push({ img: './header-list/Gadgets/chargeGudget.svg', name: "Зарядки для гаджетів" })
-      this.listCategoryItem.push({ img: './header-list/Gadgets/speakers.svg', name: "Портативні колонки" })
-      this.listCategoryItem.push({ img: './header-list/Gadgets/Action-camera.svg', name: "Action камери" })
-      this.listCategoryItem.push({ img: './header-list/Gadgets/Quadcoper.svg', name: "Квадробери" })
     }
     if(element.classList.contains("Computers")){
       this.listCategoryItem = []
-      this.listCategoryItem.push({ img: './header-list/Gadgets/iphone-svg.svg', name: "asd" })
-      this.listCategoryItem.push({ img: './header-list/Gadgets/iphone-box.svg', name: "sad" })
-      this.listCategoryItem.push({ img: './header-list/Gadgets/head-set.svg', name: "sad" })
-      this.listCategoryItem.push({ img: './header-list/Gadgets/smart-watch.svg', name: "sad і браслети" })
-      this.listCategoryItem.push({ img: './header-list/Gadgets/power-bank.svg', name: "sad" })
-      this.listCategoryItem.push({ img: './header-list/Gadgets/chargeGudget.svg', name: "ads для гаджетів" })
-      this.listCategoryItem.push({ img: './header-list/Gadgets/speakers.svg', name: "dsa колонки" })
-      this.listCategoryItem.push({ img: './header-list/Gadgets/Action-camera.svg', name: "dsa камери" })
-      this.listCategoryItem.push({ img: './header-list/Gadgets/Quadcoper.svg', name: "ad" })
+      
     }
   }
   leaveOfElement() {
