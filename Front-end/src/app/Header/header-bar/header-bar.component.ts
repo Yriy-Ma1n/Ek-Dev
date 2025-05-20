e3d03d7e74df2eb540b1751e4c82f4cb3deda017
Andrei Pustovoit <andreipustovoit6@gmail.com>
2025-05-20 20:16:48 +0300

    fix components in header-bar

--- Front-end/src/app/Header/header-bar/header-bar.component.ts
+++ Front-end/src/app/Header/header-bar/header-bar.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
 import { LogoComponent } from '../logo/logo.component';
 import { SearchComponent } from '../search/search.component';
-import { ThemeBtnComponent } from '../theme-btn/theme-btn.component';
-import { LanguageBtnComponent } from '../language-btn/language-btn.component';
 import { SinginAdminComponent } from '../singin-admin/singin-admin.component';
 import { CategoryListComponent } from '../category-list/category-list.component';
 import { CategoryComponent } from '../category/category.component';
+import { BasketComponent } from '../../basket/basket.component';
 
 @Component({
   selector: 'app-header-bar',
-  imports: [LogoComponent, SearchComponent, ThemeBtnComponent, LanguageBtnComponent, SinginAdminComponent, CategoryListComponent, CategoryComponent],
+  imports: [LogoComponent, SearchComponent, SinginAdminComponent, CategoryListComponent, CategoryComponent, BasketComponent],
   templateUrl: './header-bar.component.html',
   styleUrl: './header-bar.component.css'
 })
--- Front-end/src/app/basket/basket.component.spec.ts
+++ Front-end/src/app/basket/basket.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ThemeBtnComponent } from './theme-btn.component';
+import { BasketComponent } from './basket.component';
 
-describe('ThemeBtnComponent', () => {
-  let component: ThemeBtnComponent;
-  let fixture: ComponentFixture<ThemeBtnComponent>;
+describe('BasketComponent', () => {
+  let component: BasketComponent;
+  let fixture: ComponentFixture<BasketComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ThemeBtnComponent]
+      imports: [BasketComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(ThemeBtnComponent);
+    fixture = TestBed.createComponent(BasketComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
