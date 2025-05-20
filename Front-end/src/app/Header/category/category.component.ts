531ae4c36003450db630afb8d706d6ee96977dfd
Ma1n <kskiller2020@gmail.com>
2025-05-20 16:17:33 +0300

    add working category header list

--- Front-end/src/app/Header/category/category.component.ts
+++ Front-end/src/app/Header/category/category.component.ts
@@ -19,59 +19,29 @@ export class CategoryComponent {
     const element = event.target as HTMLElement
     if (element.classList.contains("list-category-container")) return
     this.hovered = false
+    element.classList.contains("Gadgets") ? this.listCategoryItem = this.categoryesData[0].Gadgets : false
+    element.classList.contains("Computers") ? this.listCategoryItem = this.categoryesData[0].Computers : false
+    element.classList.contains("photo") ? this.listCategoryItem = this.categoryesData[0].Photo : false
+    element.classList.contains("TV") ? this.listCategoryItem = this.categoryesData[0].Tv : false
+    element.classList.contains("Audio") ? this.listCategoryItem = this.categoryesData[0].Audio : false
+    element.classList.contains("Home-technik") ? this.listCategoryItem = this.categoryesData[0].HomeTechnik : false
+    element.classList.contains("Climat") ?  this.listCategoryItem = this.categoryesData[0].Climat : false
+    element.classList.contains("Home") ?  this.listCategoryItem = this.categoryesData[0].Home : false
+    element.classList.contains("Child-tovar") ? this.listCategoryItem = this.categoryesData[0].ChildTovar : false
+    element.classList.contains("Car") ?  this.listCategoryItem = this.categoryesData[0].Cars : false
+    element.classList.contains("Tool") ? this.listCategoryItem = this.categoryesData[0].Tools : false
+    element.classList.contains("Tourism") ?  this.listCategoryItem = this.categoryesData[0].Tourism : false
+    element.classList.contains("Sport") ? this.listCategoryItem = this.categoryesData[0].Sport : false
+    element.classList.contains("Fashion") ?  this.listCategoryItem = this.categoryesData[0].FashionBeauti : false
+  }
+leaveOfElement() {
+  this.hovered = true
 
-    if (element.classList.contains("Gadgets")) {
-      this.listCategoryItem = this.categoryesData[0].Gadgets
-    }
-    if(element.classList.contains("Computers")){
-      this.listCategoryItem = this.categoryesData[0].Computers
-    }
-    if(element.classList.contains("photo")){
-      this.listCategoryItem = this.categoryesData[0].Photo
-    }
-     if(element.classList.contains("TV")){
-      this.listCategoryItem = this.categoryesData[0].Tv
-    }
-    if(element.classList.contains("Audio")){
-      this.listCategoryItem = this.categoryesData[0].Audio
-    }
-    if(element.classList.contains("Home-technik")){
-      this.listCategoryItem = this.categoryesData[0].HomeTechnik
-    }
-    if(element.classList.contains("Climat")){
-      this.listCategoryItem = this.categoryesData[0].Climat
-    }
-     if(element.classList.contains("Home")){
-      this.listCategoryItem = this.categoryesData[0].Home
-    }
-    if(element.classList.contains("Child-tovar")){
-      this.listCategoryItem = this.categoryesData[0].ChildTovar
-    }
-    if(element.classList.contains("Car")){
-      this.listCategoryItem = this.categoryesData[0].Cars
-    }
-    if(element.classList.contains("Tool")){
-      this.listCategoryItem = this.categoryesData[0].Tools
-    }
-    if(element.classList.contains("Tourism")){
-      this.listCategoryItem = this.categoryesData[0].Tourism
-    }
-     if(element.classList.contains("Sport")){
-      this.listCategoryItem = this.categoryesData[0].Sport
-    }
-    if(element.classList.contains("Fashion")){
-      this.listCategoryItem = this.categoryesData[0].FashionBeauti
-    }
-
-  }
-  leaveOfElement() {
-    this.hovered = true
-
-  }
-  itemIn(){
-    this.hovered = false
-  }
-  itemout(){
-    this.hovered = true
-  }
+}
+itemIn(){
+  this.hovered = false
+}
+itemout(){
+  this.hovered = true
+}
 }
