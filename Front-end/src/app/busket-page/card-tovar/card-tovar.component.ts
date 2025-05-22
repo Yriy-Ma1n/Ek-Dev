6a552986d87cd76e678f951fbb8d161185d5843f
Ma1n <kskiller2020@gmail.com>
2025-05-22 17:52:03 +0300

    add increment and dicrement price in tovar

--- Front-end/src/app/busket-page/card-tovar/card-tovar.component.ts
+++ Front-end/src/app/busket-page/card-tovar/card-tovar.component.ts
@@ -7,14 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './card-tovar.component.css'
 })
 export class CardTovarComponent {
-count:number = 0
+count:number = 1;
+price:number = 20;
+constPrice:number = 20;
 
  plusButton(){
-  
   this.count += 1
+  this.price += this.constPrice
  }
  minusButton(){
   if(this.count <= 0) return
   this.count -= 1
+  this.price -= this.constPrice
  }
 }
