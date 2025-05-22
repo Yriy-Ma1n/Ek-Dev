e4fb5d22c4903ade2f0a03f5f6769e777ed5555f
Andrei Pustovoit <andreipustovoit6@gmail.com>
2025-05-22 21:29:44 +0300

    fix header-bar in busket

--- Front-end/src/app/busket-page/busket-page.component.ts
+++ Front-end/src/app/busket-page/busket-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CardTovarComponent } from './card-tovar/card-tovar.component';
+import { HeaderBarComponent } from '../shared/components/header-bar/header-bar.component';
 
 @Component({
   selector: 'app-busket-page',
-  imports: [CardTovarComponent],
+  imports: [CardTovarComponent, HeaderBarComponent],
   templateUrl: './busket-page.component.html',
   styleUrl: './busket-page.component.css'
 })
