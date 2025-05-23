9a5a147cd6acb0b26cc46417d81a97a40ab78b80
Ma1n <kskiller2020@gmail.com>
2025-05-23 20:01:43 +0300

    add 404 page component

---
+++ Front-end/src/app/page404/page-nf/page-nf.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-nf',
+  imports: [],
+  templateUrl: './page-nf.component.html',
+  styleUrl: './page-nf.component.css'
+})
+export class PageNfComponent {
+
+}
