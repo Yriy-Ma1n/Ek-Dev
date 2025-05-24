19538bab2774e3c58e2b17adccf69bb1746b17ad
Andrei Pustovoit <andreipustovoit6@gmail.com>
2025-05-24 19:53:20 +0300

    add router at page admin

--- Front-end/src/app/app.routes.ts
+++ Front-end/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { MainSectionComponent } from './main-section/main-section.component';
 import { BusketPageComponent } from './busket-page/busket-page.component';
 import { LoadingScreenComponent } from './loading-screen/loading-screen.component';
+import { SinginAdminComponent } from './Header/singin-admin/singin-admin.component';
 
 export const routes: Routes = [
   {
@@ -15,5 +16,9 @@ export const routes: Routes = [
   {
     path:'busket',
     component:BusketPageComponent,
+  },
+  {
+    path:'admin',
+    component:SinginAdminComponent,
   }
 ];
