45c58a6e68f39387a92524c4a10f46afaf9aae18
Andrei Pustovoit <andreipustovoit6@gmail.com>
2025-05-24 20:06:11 +0300

    download data

--- Front-end/src/app/app.component.ts
+++ Front-end/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { HeaderBarComponent } from './shared/components/header-bar/header-bar.component';
-<<<<<<< HEAD
 import { RouterOutlet } from '@angular/router';
 import { MainSectionComponent } from "./main-section/main-section.component";
 
@@ -8,14 +7,7 @@ import { BusketPageComponent } from "./busket-page/busket-page.component";
 
 @Component({
   selector: 'app-root',
-  imports: [ HeaderBarComponent, RouterOutlet],
-=======
-import { RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
-
-@Component({
-  selector: 'app-root',
-  imports: [ RouterModule, RouterLinkActive, HeaderBarComponent, RouterOutlet],
->>>>>>> d4ffbc1bdb69390b14f2f106ad1d6a92ca866913
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
