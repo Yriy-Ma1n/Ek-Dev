676874b44007e6446231e9689579ff0354b5d8e5
Ma1n <kskiller2020@gmail.com>
2025-05-25 14:00:13 +0300

    add svg arrow and add page card

--- Front-end/src/app/shared/components/product-card-inner/product-card-inner.component.ts
+++ Front-end/src/app/shared/components/product-card-inner/product-card-inner.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ProductCardComponent } from '../product-card/product-card.component';
+import { ProductCardCharacteristicsComponent } from '../product-card-characteristics/product-card-characteristics.component';
 
 @Component({
   selector: 'app-product-card-inner',
-  imports: [],
+  imports: [ProductCardCharacteristicsComponent],
   templateUrl: './product-card-inner.component.html',
   styleUrl: './product-card-inner.component.css'
 })
