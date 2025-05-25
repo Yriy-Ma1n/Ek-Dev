bccda2aafdc803f536db02e0bb9352288a77a9e2
Ma1n <kskiller2020@gmail.com>
2025-05-25 14:44:01 +0300

    add style for input and send button

--- Front-end/src/app/shared/components/product-card-inner/product-card-inner.component.ts
+++ Front-end/src/app/shared/components/product-card-inner/product-card-inner.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ProductCardComponent } from '../product-card/product-card.component';
 import { ProductCardCharacteristicsComponent } from '../product-card-characteristics/product-card-characteristics.component';
+import { HeaderBarComponent } from "../header-bar/header-bar.component";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-card-inner',
-  imports: [ProductCardCharacteristicsComponent],
+  imports: [ProductCardCharacteristicsComponent, HeaderBarComponent],
   templateUrl: './product-card-inner.component.html',
   styleUrl: './product-card-inner.component.css'
 })
 export class ProductCardInnerComponent {
-
+  router = inject(Router)
+  backToMainPage(){
+    this.router.navigate(["/Home"])
+  }
 }
