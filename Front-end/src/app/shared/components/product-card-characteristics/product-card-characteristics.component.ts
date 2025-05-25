890a0444dfcc17e22dfe49decbaa5fec28f2f250
Ma1n <kskiller2020@gmail.com>
2025-05-25 11:24:53 +0300

    add component for re-use

---
+++ Front-end/src/app/shared/components/product-card-characteristics/product-card-characteristics.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-card-characteristics',
+  imports: [],
+  templateUrl: './product-card-characteristics.component.html',
+  styleUrl: './product-card-characteristics.component.css'
+})
+export class ProductCardCharacteristicsComponent {
+
+}
