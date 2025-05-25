2751187d60a6c2d09c433767d6df2f1d8dca9482
Ma1n <kskiller2020@gmail.com>
2025-05-25 15:11:00 +0300

    add pipe for change bad work

--- Front-end/src/app/shared/components/product-card-inner/product-card-inner.component.ts
+++ Front-end/src/app/shared/components/product-card-inner/product-card-inner.component.ts
@@ -3,16 +3,27 @@ import { ProductCardComponent } from '../product-card/product-card.component';
 import { ProductCardCharacteristicsComponent } from '../product-card-characteristics/product-card-characteristics.component';
 import { HeaderBarComponent } from "../header-bar/header-bar.component";
 import { Router } from '@angular/router';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-product-card-inner',
-  imports: [ProductCardCharacteristicsComponent, HeaderBarComponent],
+  imports: [ProductCardCharacteristicsComponent, HeaderBarComponent, ReactiveFormsModule],
   templateUrl: './product-card-inner.component.html',
   styleUrl: './product-card-inner.component.css'
 })
 export class ProductCardInnerComponent {
   router = inject(Router)
-  backToMainPage(){
+
+  commentInput = new FormControl("", [
+    Validators.required
+  ])
+
+  backToMainPage() {
     this.router.navigate(["/Home"])
+  }
+  addComment(){
+    if(this.commentInput.valid){
+      console.log('send')
+    }
   }
 }
---
+++ Front-end/src/app/shared/pipes/bad-word.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'badWord'
+})
+export class BadWordPipe implements PipeTransform {
+
+  transform(value: unknown, ...args: unknown[]): unknown {
+    return null;
+  }
+
+}
