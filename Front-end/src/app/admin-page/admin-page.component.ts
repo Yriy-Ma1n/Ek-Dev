4e192822e4b8289fc35d348fb69b24c41c149ea7
Andrei Pustovoit <andreipustovoit6@gmail.com>
2025-05-25 23:51:45 +0300

    add check password admin

--- Front-end/src/app/admin-page/admin-page.component.ts
+++ Front-end/src/app/admin-page/admin-page.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { HeaderBarComponent } from '../shared/components/header-bar/header-bar.component';
 import { AndminPasswordComponent } from './andmin-password/andmin-password.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-admin-page',
-  imports: [HeaderBarComponent, AndminPasswordComponent],
+  imports: [HeaderBarComponent, AndminPasswordComponent, NgIf],
   templateUrl: './admin-page.component.html',
   styleUrl: './admin-page.component.css'
 })
 export class AdminPageComponent {
-
+  modal: boolean = true;
+  constructor(){
+    this.modal = true;
+  }
+  @Input()
+  inputValueBul(event:boolean){
+    this.modal = event;
+  };
 }
--- Front-end/src/app/admin-page/andmin-password/andmin-password.component.ts
+++ Front-end/src/app/admin-page/andmin-password/andmin-password.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-andmin-password',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './andmin-password.component.html',
   styleUrl: './andmin-password.component.css'
 })
 export class AndminPasswordComponent {
+  inputItem = new FormControl();
+  adminPassword: string = "admin";
+  inputValue: string = "";
+  inputValueBul: boolean = true;
+  @Output() inputEmiter = new EventEmitter <boolean>();
 
+  getValueInput(inputItem: HTMLInputElement){
+    this.inputValue = inputItem.value;
+    this.checkValueInput();
+    this.inputEmiter.emit(this.inputValueBul);
+  };
+
+  checkValueInput(){
+    if(this.inputValue === this.adminPassword){
+      this.inputValueBul = false;
+    }else{
+      this.inputValueBul = true;
+    }
+  };
 }
