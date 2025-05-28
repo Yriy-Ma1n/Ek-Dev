c551b7e52f3be6ee0d8b2cae07954fe1c80ca970
Andrei Pustovoit <andreipustovoit6@gmail.com>
2025-05-28 23:25:28 +0300

    add button to Home in admin-page

--- Front-end/src/app/admin-page/andmin-password/andmin-password.component.ts
+++ Front-end/src/app/admin-page/andmin-password/andmin-password.component.ts
@@ -2,10 +2,11 @@ import { Component, EventEmitter, Output } from '@angular/core';
 import { FormControl, ReactiveFormsModule} from '@angular/forms';
 import { PasswordValidator } from './Passwors.validators.directive';
 import { NgClass, NgIf } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-andmin-password',
-  imports: [ReactiveFormsModule, NgIf, NgClass],
+  imports: [ReactiveFormsModule, NgIf, NgClass, RouterLink],
   templateUrl: './andmin-password.component.html',
   styleUrl: './andmin-password.component.css'
 })
