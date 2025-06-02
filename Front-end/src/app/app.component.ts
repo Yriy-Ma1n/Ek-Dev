9ab477a1b01177898cde52501938d146f14a1052
LysenkoN <anastasialysenko75@gmail.com>
2025-06-02 14:57:11 +0300

    Resolve conflicts

--- Front-end/src/app/app.component.ts
+++ Front-end/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderBarComponent } from './shared/components/header-bar/header-bar.component';
 import { RouterOutlet } from '@angular/router';
+import { PopularModelsComponent } from './popular-models/popular-models.component';
 import { MainSectionComponent } from "./main-section/main-section.component";
 
 import { BusketPageComponent } from "./busket-page/busket-page.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ HeaderBarComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
