d825483a0f4149b8cc35d926071d581a82745e53
Ma1n <kskiller2020@gmail.com>
2025-06-05 10:15:22 +0300

    add tovar-list component and register in routs

---
+++ Front-end/src/app/main-section/tovar-list/tovar-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tovar-list',
+  imports: [],
+  templateUrl: './tovar-list.component.html',
+  styleUrl: './tovar-list.component.css'
+})
+export class TovarListComponent {
+
+}
