f2d65f1531df14d3995e9a113df3318cd0760c97
Andrei Pustovoit <andreipustovoit6@gmail.com>
2025-06-14 14:10:08 +0300

    create component create-tovat

---
+++ Front-end/src/app/admin-page/create-tovar/create-tovar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-tovar',
+  imports: [],
+  templateUrl: './create-tovar.component.html',
+  styleUrl: './create-tovar.component.css'
+})
+export class CreateTovarComponent {
+
+}
