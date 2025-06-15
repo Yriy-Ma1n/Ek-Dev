8da67613eac9cb5f2d6a841b0aa469fbf0385cd3
Andrei Pustovoit <andreipustovoit6@gmail.com>
2025-06-15 16:48:23 +0300

    add post new tovar

--- Front-end/src/app/admin-page/create-tovar/create-tovar.component.ts
+++ Front-end/src/app/admin-page/create-tovar/create-tovar.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create-tovar',
-  imports: [],
+  imports: [ReactiveFormsModule,],
   templateUrl: './create-tovar.component.html',
   styleUrl: './create-tovar.component.css'
 })
 export class CreateTovarComponent {
+  http = inject(HttpClient);
+  newCart = {}
 
+  sendData(name:HTMLInputElement, description:HTMLTextAreaElement, url:HTMLInputElement, cost:HTMLInputElement) {
+    this.newCart = {
+      "img": url.value,
+      "name": name.value,
+      "cost": cost.value,
+      "description":[description.value]
+    }
+
+    return this.http.post("localhost:5501/addProduct", this.newCart);
+  }
 }
