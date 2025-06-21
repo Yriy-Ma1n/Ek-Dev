800466923130dbcd38e17a58091e7877d75d7b15
Andrei Pustovoit <andreipustovoit6@gmail.com>
2025-06-21 14:45:49 +0300

    ass style of validation

--- Front-end/src/app/admin-page/create-tovar/create-tovar.component.ts
+++ Front-end/src/app/admin-page/create-tovar/create-tovar.component.ts
@@ -2,15 +2,20 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject} from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ValidatorUrl } from './cart-validator.directive';
+import { NgClass, NgIf} from '@angular/common';
 
 
 @Component({
   selector: 'app-create-tovar',
-  imports: [ReactiveFormsModule,],
+  imports: [ReactiveFormsModule, NgIf, NgClass],
   templateUrl: './create-tovar.component.html',
   styleUrl: './create-tovar.component.css'
 })
 export class CreateTovarComponent {
+  inputCost = new FormControl("",[
+      Validators.pattern(/^[0-9]+$/)
+    ]);
+
   form = new FormGroup({
     inputName: new FormControl("",[
       Validators.pattern(/^[A-Za-zА-Яа-яЁё\s]+$/)
@@ -21,12 +26,11 @@ export class CreateTovarComponent {
     inputUrl: new FormControl("", [
       ValidatorUrl()
     ]),
-    inputCost: new FormControl("",[
-      Validators.pattern(/^[0-9]+$/)
-    ]),
+    inputCost: this.inputCost
   });
 
   http = inject(HttpClient);
+  inputNgifCost = false;
 
   sendData(name:HTMLInputElement, description:HTMLTextAreaElement, url:HTMLInputElement, cost:HTMLInputElement) {
     const newCart = {
@@ -36,6 +40,14 @@ export class CreateTovarComponent {
       description:[description.value]
     }
 
-    this.http.post("http://localhost:5500/addProduct", newCart).subscribe(data=>console.log(data));
+    if(this.inputCost.invalid){
+      this.inputNgifCost = true;
+    }else{
+      this.inputNgifCost = false;
+    }
+
+    // if(this.form.valid){
+    //   this.http.post("http://localhost:5500/addProduct", newCart).subscribe(data=>console.log(data));
+    // }
   }
 }
