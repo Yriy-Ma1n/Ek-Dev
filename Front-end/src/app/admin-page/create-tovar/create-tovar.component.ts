98cbb1470a8e20dfae9ed8e08f75aa517517ce1a
Andrei Pustovoit <andreipustovoit6@gmail.com>
2025-06-23 12:28:22 +0300

    update validation

--- Front-end/src/app/admin-page/create-tovar/create-tovar.component.ts
+++ Front-end/src/app/admin-page/create-tovar/create-tovar.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, inject} from '@angular/core';
+import { Component, inject, input} from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ValidatorUrl } from './cart-validator.directive';
 import { NgClass, NgIf} from '@angular/common';
@@ -12,25 +12,30 @@ import { NgClass, NgIf} from '@angular/common';
   styleUrl: './create-tovar.component.css'
 })
 export class CreateTovarComponent {
+  inputName =  new FormControl("",[
+      Validators.pattern(/^[A-Za-zА-Яа-яЁё\s]+$/),
+      Validators.required
+    ]);
+  textAreaDescription = new FormControl("",[
+      Validators.pattern(/^[a-zA-Zа-яА-ЯёЁ0-9 ]+$/),
+      Validators.required
+    ]);
   inputCost = new FormControl("",[
-      Validators.pattern(/^[0-9]+$/)
+      Validators.pattern(/^[0-9]+$/),
+      Validators.required
     ]);
 
   form = new FormGroup({
-    inputName: new FormControl("",[
-      Validators.pattern(/^[A-Za-zА-Яа-яЁё\s]+$/)
-    ]),
-    textAreaDescription: new FormControl("",[
-      Validators.pattern(/^[a-zA-Zа-яА-ЯёЁ0-9 ]+$/)
-    ]),
-    inputUrl: new FormControl("", [
-      ValidatorUrl()
-    ]),
+    inputName: this.inputName,
+    textAreaDescription: this.textAreaDescription,
     inputCost: this.inputCost
   });
 
   http = inject(HttpClient);
   inputNgifCost = false;
+  inputNgName = false;
+  textAreaNgDescription = false;
+  inputNgUrl = false;
 
   sendData(name:HTMLInputElement, description:HTMLTextAreaElement, url:HTMLInputElement, cost:HTMLInputElement) {
     const newCart = {
@@ -46,8 +51,29 @@ export class CreateTovarComponent {
       this.inputNgifCost = false;
     }
 
-    // if(this.form.valid){
-    //   this.http.post("http://localhost:5500/addProduct", newCart).subscribe(data=>console.log(data));
-    // }
+    if(this.inputName.invalid){
+      this.inputNgName = true;
+    }else{
+      this.inputNgName = false;
+    }
+
+    if(this.textAreaDescription.invalid){
+      this.textAreaNgDescription = true;
+    }else{
+      this.textAreaNgDescription = false;
+    }
+
+    const img = new Image();
+    img.src = url.value;
+    img.onerror = ()=>{
+      this.inputNgUrl = true;
+    }
+    img.onload = ()=>{
+      this.inputNgUrl = false;
+    }
+
+    if(this.form.valid){
+      this.http.post("http://localhost:5500/addProduct", newCart).subscribe(data=>console.log(data));
+    }
   }
 }
