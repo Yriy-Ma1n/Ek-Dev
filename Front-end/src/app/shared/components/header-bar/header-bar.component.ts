7984764b7676647d44e9d8c642f3f71bf4f54578
LysenkoN <anastasialysenko75@gmail.com>
2025-07-02 15:09:59 +0300

    add theme-toggle

--- Front-end/src/app/shared/components/header-bar/header-bar.component.ts
+++ Front-end/src/app/shared/components/header-bar/header-bar.component.ts
@@ -4,11 +4,12 @@ import { SearchComponent } from '../../../Header/search/search.component';
 import { SinginAdminComponent } from '../../../Header/singin-admin/singin-admin.component';
 import { BasketComponent } from '../../../basket/basket.component';
 import { CategoryComponent } from '../../../Header/category/category.component';
+import { AppToggleThemeComponent } from '../../../Header/app-toggle-theme/app-toggle-theme.component';
 
 
 @Component({
   selector: 'app-header-bar',
-  imports: [LogoComponent, SearchComponent, SinginAdminComponent, BasketComponent, CategoryComponent],
+  imports: [LogoComponent, SearchComponent, SinginAdminComponent, AppToggleThemeComponent, BasketComponent, CategoryComponent],
   templateUrl: './header-bar.component.html',
   styleUrl: './header-bar.component.css'
 })
