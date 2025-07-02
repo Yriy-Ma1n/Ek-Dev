5824a62e945b72ea56ff81e6f6b56108f5616fda
Andrei Pustovoit <andreipustovoit6@gmail.com>
2025-07-02 15:49:30 +0300

    fix conflickt

--- Front-end/src/app/shared/components/header-bar/header-bar.component.ts
+++ Front-end/src/app/shared/components/header-bar/header-bar.component.ts
@@ -4,20 +4,12 @@ import { SearchComponent } from '../../../Header/search/search.component';
 import { SinginAdminComponent } from '../../../Header/singin-admin/singin-admin.component';
 import { BasketComponent } from '../../../basket/basket.component';
 import { CategoryComponent } from '../../../Header/category/category.component';
-<<<<<<< HEAD
 import { SingInComponent } from '../../../sing-in/sing-in.component';
-=======
 import { ChangeCurrencyComponent } from '../../../change-currency/change-currency.component';
->>>>>>> 34f7e7b63f6aff7e7fe0ddf77fd401b60c23cc3d
-
 
 @Component({
   selector: 'app-header-bar',
-<<<<<<< HEAD
-  imports: [LogoComponent, SearchComponent, SinginAdminComponent, BasketComponent, CategoryComponent, SingInComponent],
-=======
-  imports: [LogoComponent, SearchComponent, SinginAdminComponent, BasketComponent, CategoryComponent, ChangeCurrencyComponent],
->>>>>>> 34f7e7b63f6aff7e7fe0ddf77fd401b60c23cc3d
+  imports: [LogoComponent, SearchComponent, SinginAdminComponent, BasketComponent, CategoryComponent, SingInComponent, ChangeCurrencyComponent],
   templateUrl: './header-bar.component.html',
   styleUrl: './header-bar.component.css'
 })
