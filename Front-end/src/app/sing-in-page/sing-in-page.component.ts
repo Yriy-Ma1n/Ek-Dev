249f1cf786cdd169550d4ac77c1b2104ffe0a116
Ma1n <kskiller2020@gmail.com>
2025-07-05 16:10:29 +0300

    add reload when user sing in account

--- Front-end/src/app/sing-in-page/sing-in-page.component.ts
+++ Front-end/src/app/sing-in-page/sing-in-page.component.ts
@@ -3,18 +3,19 @@ import { HeaderBarComponent } from '../shared/components/header-bar/header-bar.c
 import { Router } from '@angular/router';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-sing-in-page',
-  imports: [HeaderBarComponent, ReactiveFormsModule],
+  imports: [HeaderBarComponent, ReactiveFormsModule, NgClass],
   templateUrl: './sing-in-page.component.html',
   styleUrl: './sing-in-page.component.css'
 })
 export class SingInPageComponent {
   router = inject(Router)
   http = inject(HttpClient)
-  pass: string = '1'
 
+  hideError: boolean = true
   logInForm = new FormGroup({
     name: new FormControl("", [Validators.required]),
     password: new FormControl("", [Validators.required])
@@ -28,12 +29,19 @@ export class SingInPageComponent {
       `http://localhost:5500/login`,
       { name: name.value, password: password.value },
       { withCredentials: true }
-    ).subscribe(() => {
-      
-      location.reload()
-      
+    ).subscribe((data) => {
+      this.router.navigate(['/Home'])
+      this.hideError = true
+      setTimeout(() => {
+        location.reload()
+      }, 100)
+
+    }, (error) => {
+      if (error) {
+        this.hideError = false
+      }
     })
-    this.router.navigate(['/Home'])
+
   }
 
 }
