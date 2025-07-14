b3df13965854198aadc6ab451de0991aae197a49
Andrei Pustovoit <andreipustovoit6@gmail.com>
2025-07-14 17:17:14 +0300

    update some adaptive in burger menu

--- Front-end/src/app/Header/burger-menu/burger-menu.component.ts
+++ Front-end/src/app/Header/burger-menu/burger-menu.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { SingInComponent } from '../../sing-in/sing-in.component';
+import { ChangeCurrencyComponent } from '../../change-currency/change-currency.component';
+import { BusketPageComponent } from '../../busket-page/busket-page.component';
+import { BasketComponent } from '../../basket/basket.component';
+import { SinginAdminComponent } from '../singin-admin/singin-admin.component';
 
 @Component({
   selector: 'app-burger-menu',
-  imports: [],
+  imports: [SingInComponent, ChangeCurrencyComponent, BasketComponent, SinginAdminComponent],
   templateUrl: './burger-menu.component.html',
   styleUrl: './burger-menu.component.css'
 })
 export class BurgerMenuComponent {
+  burgerMenuScreen = 'none';
+  bgColor = 'transparent';
 
+  onClickBurgerMenu(aside:HTMLElement){
+    aside.classList.remove('close')
+    aside.classList.add('open')
+    this.burgerMenuScreen = 'block'
+    this.bgColor = '#00000052'
+  }
+  onClickCross(aside:HTMLElement){
+    aside.classList.remove('open')
+    aside.classList.add('close')
+    setTimeout(()=>{
+        this.burgerMenuScreen = 'none'
+    },300)
+    this.bgColor = 'transparent'
+  }
 }
